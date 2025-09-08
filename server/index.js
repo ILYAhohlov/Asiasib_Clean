857e5b16ea7d367c4753b949d27a8f5efef5424d
Ilya Hohlov <ilya.o.hohlov@gmail.com>
2025-09-08 16:20:29 +0700

    Fix CORS for asiasib-clean.vercel.app

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS
 app.use(cors({
-  origin: ['https://asiasib.vercel.app', 'http://localhost:3000'],
+  origin: ['https://asiasib-clean.vercel.app', 'https://asiasib.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
